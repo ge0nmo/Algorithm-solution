1d1caa5e44b54ac5965000cc195f8421432e7682
ge0nmo <ands0927@naver.com>
2023-02-15 16:37:42 +0900

    Sum of consecutive subsequence

---
+++ src/efficiency/slidingwindow/Subsequence.java
@@ -0,0 +1,50 @@
+package efficiency.slidingwindow;
+
+import java.io.*;
+import java.util.*;
+
+public class Subsequence
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int num = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution1(num, arr));
+    }
+
+    public static int solution1(int num, int arr[])
+    {
+        int lt = 0;
+        int sum = 0;
+        int count = 0;
+
+        for(int rt = 0; rt < arr.length; rt++)
+        {
+            sum += arr[rt];
+            if(sum == num)
+                ++count;
+
+            while(sum >= num)
+            {
+                sum -= arr[lt++];
+                if(sum == num)
+                    ++count;
+            }
+
+        }
+
+        return count;
+    }
+}
