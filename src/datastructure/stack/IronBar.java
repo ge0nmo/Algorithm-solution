673abd0261bb5d06ddabc5e48667665b783f7a3e
ge0nmo <ands0927@naver.com>
2023-02-18 19:42:03 +0900

    Iron bar

--- src/datastructure/stack/IronBar.java
+++ src/datastructure/stack/IronBar.java
@@ -14,6 +14,13 @@ public class IronBar
 
     public static int solution1(String str)
     {
+        //문자열을 반복문으로 돌린다.
+        //여는 괄호가 나오면 스택에 push
+        //닫는 괄호가 나오면 pop 해준 뒤 스택의 가장 윗 부분과 비교
+        //여는 괄호면 레이저이기 때문에 스택의 크기만큼 더해준다
+        //닫는 괄호라면 +1만 해준다
+        //최종 count 개수를 리턴
+
         Stack<Character> stack = new Stack<>();
         int count = 0;
 
