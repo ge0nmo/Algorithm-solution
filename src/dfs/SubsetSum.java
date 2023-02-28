1f66f83a360760cd33fcda48a0f56384a7e6e6ec
ge0nmo <ands0927@naver.com>
2023-02-28 18:23:39 +0900

    합이 같은 부분집합(DFS)

--- src/dfs/SubsetSum.java
+++ src/dfs/SubsetSum.java
@@ -5,16 +5,23 @@ import java.io.*;
 
 public class SubsetSum
 {
+    //원소의 개수만큼 깊이 우선 탐색을 돌린다
+    //체크 값이 false 일때 돌리고 true 라면 바로 리턴
+    //원소의 개수와 깊이 레벨이 같다면 total - sum 값이 sum 인지 체크
+    //맞다면 answer = YES 후에 뒤의 깊이들은 체크할 필요가 없으므로 체크 값을 true 로 변경
+    //틀리다면 answer = NO
+
+    static int total = 0;
+    static int n;
     static String answer = "NO";
-    static int n, total = 0;
-    static boolean flag = false;
+    static boolean check = false;
 
     public static void DFS(int L, int sum, int arr[])
     {
-        if(flag)
+        if(check)
             return;
 
-        if(sum > total/2)
+        if(sum > total/ 2)
             return;
 
         if(L == n)
@@ -22,7 +29,7 @@ public class SubsetSum
             if((total - sum) == sum)
             {
                 answer = "YES";
-                flag = true;
+                check = true;
             }
         }
 
@@ -48,6 +55,7 @@ public class SubsetSum
 
         DFS(0, 0, arr);
 
-        System.out.print(answer);
+        System.out.println(answer);
+
     }
 }
