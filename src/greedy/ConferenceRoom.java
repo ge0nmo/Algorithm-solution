883f9cc2a877bf089c3e21ed12046764d27efd10
ge0nmo <ands0927@naver.com>
2023-03-11 18:10:14 +0900

    회의실 배정

--- src/greedy/ConferenceRoom.java
+++ src/greedy/ConferenceRoom.java
@@ -4,6 +4,12 @@ import java.io.*;
 import java.util.*;
 public class ConferenceRoom
 {
+    //회의라는 클래스를 생성해서 시작 시간과 종료 시간을 변수로 설정한다.
+    //정렬을 할 때 빨리 회의가 종료되는 순서대로 정렬을 하고
+    //종료 시간이 같은 경우 빨리 시작하는 회의 순으로 정렬을 한다
+    //0을 변수로 설정하고 모든 회의를 반복문으로 돌린다
+    //회의 시작 시간이 변수보다 큰 경우 변수를 해당 회의의 종료 시간으로 변경하고 카운트를 해준다
+
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
